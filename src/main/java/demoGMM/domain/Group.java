dff9d0bc331c040b06b3fc558f7cf84fde2c6561
genmymodel <help@genmymodel.com>
2013-05-21 11:35:32 +0200

    commit by genmymodel from www.genmymodel.com: v20130521_113530

---
+++ src/main/java/demoGMM/domain/Group.java
@@ -0,0 +1,165 @@
+package demoGMM.domain;
+import java.util.Set;
+import java.util.HashSet;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+@javax.persistence.Entity
+public class Group
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	@javax.persistence.Column(nullable = false)
+	protected String name;
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	@javax.persistence.OneToMany(mappedBy = "group")
+	protected Set<User> users;
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	@javax.persistence.Id
+	@javax.persistence.Column(nullable = false)
+	protected Long id;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Group(){
+		
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public String getName() {
+		return this.name;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public Set<User> getUsers() {
+		if(this.users == null) {
+				this.users = new HashSet<User>();
+		}
+		return (Set<User>) this.users;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public long getId() {
+		return this.id;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void addAllUsers(Set<User> newUsers) {
+		if (this.users == null) {
+			this.users = new HashSet<User>();
+		}
+		for (User tmp : newUsers)
+			tmp.setGroup(this);
+			
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void removeAllUsers(Set<User> newUsers) {
+		if(this.users == null) {
+			return;
+		}
+		
+		this.users.removeAll(newUsers);	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setName(String myName) {
+		this.name = myName;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void addUsers(User newUsers) {
+		if(this.users == null) {
+			this.users = new HashSet<User>();
+		}
+		
+		if (this.users.add(newUsers))
+			newUsers.basicSetGroup(this);	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetName() {
+		this.name = "";	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void removeUsers(User oldUsers) {
+		if(this.users == null)
+			return;
+		
+		if (this.users.remove(oldUsers))
+			oldUsers.unsetGroup();
+			
+	}
+	
+}
+
---
+++ src/main/java/demoGMM/domain/User.java
@@ -0,0 +1,304 @@
+package demoGMM.domain;
+import java.util.Date;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+@javax.persistence.Entity
+public abstract class User
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	@javax.persistence.Column(nullable = false)
+	protected String login;
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	@javax.persistence.Column(nullable = false)
+	protected String firstname;
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	@javax.persistence.Column(nullable = false)
+	protected String lastname;
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	@javax.persistence.Column(nullable = false)
+	protected String email;
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	@javax.persistence.Temporal(javax.persistence.TemporalType.DATE)
+	@javax.persistence.Column(nullable = false)
+	protected Date birthday;
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	
+	@javax.persistence.ManyToOne
+	@javax.persistence.JoinColumn(nullable = false)
+	protected Group group;
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	@javax.persistence.Id
+	@javax.persistence.Column(nullable = false)
+	protected Long id;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public User(){
+		
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void basicSetGroup(Group myGroup) {
+		if (this.group != myGroup) {
+			if (myGroup != null){
+				if (this.group != myGroup) {
+					Group oldgroup = this.group;
+					this.group = myGroup;
+					if (oldgroup != null)
+						oldgroup.removeUsers(this);
+				}
+			}
+		}	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public String getLogin() {
+		return this.login;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public String getFirstname() {
+		return this.firstname;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public String getLastname() {
+		return this.lastname;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public String getEmail() {
+		return this.email;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public Date getBirthday() {
+		return this.birthday;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public Group getGroup() {
+		return this.group;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public long getId() {
+		return this.id;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setLogin(String myLogin) {
+		this.login = myLogin;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setFirstname(String myFirstname) {
+		this.firstname = myFirstname;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setLastname(String myLastname) {
+		this.lastname = myLastname;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setEmail(String myEmail) {
+		this.email = myEmail;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setBirthday(Date myBirthday) {
+		this.birthday = myBirthday;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setGroup(Group myGroup) {
+		this.basicSetGroup(myGroup);
+		myGroup.addUsers(this);	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetLogin() {
+		this.login = "";	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetFirstname() {
+		this.firstname = "";	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetLastname() {
+		this.lastname = "";	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetEmail() {
+		this.email = "";	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetBirthday() {
+		this.birthday = new Date();	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetGroup() {
+		if (this.group == null)
+			return;
+		Group oldgroup = this.group;
+		this.group = null;
+		oldgroup.removeUsers(this);	
+	}
+	
+}
+
